9e50369e40f987303881d41b809f93debcc260aa
Abdulaziz Albahar <67667005+azooz2003-bit@users.noreply.github.com>
2023-02-27 15:23:54 -0500

    Initial Commit

---
+++ Assignment2_TodoListApp/Assignment2_TodoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment2_TodoListAppApp.swift
+//  Assignment2_TodoListApp
+//
+//  Created by Abdulaziz Albahar on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Assignment2_TodoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
